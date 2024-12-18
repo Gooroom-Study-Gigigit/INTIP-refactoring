f6556ff17ef39df986b97a55a4947a0538df92a8
yohan <rmvmf96@gmail.com>
2024-12-18 15:18:09 +0900

    folder 서비스 코드의 가독성을 위해 메서드와 DisplayName  문구 수정

--- src/test/java/kr/inuappcenterportal/inuportal/domain/folder/service/FolderServiceTest.java
+++ src/test/java/kr/inuappcenterportal/inuportal/domain/folder/service/FolderServiceTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,19 +38,6 @@ class FolderServiceTest {
     private Folder folder;
     private FolderDto folderDto;
 
-
-    @Transactional
-    public Long createFolder(Member member, FolderDto folderDto ){
-        return folderRepository.save(Folder.builder().name(folderDto.getName()).member(member).build()).getId();
-    }
-
-    @Transactional
-    public Long updateFolder(Long folderId, FolderDto folderDto){
-        Folder folder = folderRepository.findById(folderId).orElseThrow(()->new MyException(MyErrorCode.FOLDER_NOT_FOUND));
-        folder.update(folderDto.getName());
-        return folderId;
-    }
-
     @BeforeEach
     void setUp() {
         member = new Member("20231001", "member1", List.of("ROLE_USER"));
@@ -60,10 +48,10 @@ class FolderServiceTest {
     }
 
     @Test
-    @DisplayName("스크랩 폴더 생성 테스트")
+    @DisplayName("스크랩 폴더를 생성하면 생성된 폴더의 ID를 반환한다")
     void createFolder() {
         //given : createFolder메서드에서 실행되는 folderRepository.save()가 호출되면, 미리 준비된 folder 객체를 반환
-        when(folderRepository.save(any(Folder.class))).thenReturn(folder);
+        given(folderRepository.save(any(Folder.class))).willReturn(folder);
 
         //when
         Long folderId = folderService.createFolder(member, folderDto);
@@ -74,12 +62,12 @@ class FolderServiceTest {
     }
 
     @Test
-    @DisplayName("스크랩 폴더명 수정 테스트")
+    @DisplayName("존재하는 스크랩 폴더의 이름을 수정하면 수정된 폴더의 ID를 반환한다")
     void updateFolder() {
         FolderDto updateDto = new FolderDto("폴더명 수정1");
 
         // given: updateFolder메서드가 실행되면 findById를 통해 id가 존재하는지 1차 체크를 한다
-        when(folderRepository.findById(1L)).thenReturn(Optional.of(folder));
+        given(folderRepository.findById(1L)).willReturn(Optional.of(folder));
 
         // when: updateFolder 메서드 호출
         Long updatedFolderId = folderService.updateFolder(1L, updateDto);
@@ -91,10 +79,10 @@ class FolderServiceTest {
     }
 
     @Test
-    @DisplayName("updateFolder메서드 실행중에 폴더id를 찾을 수 없는 경우 테스트")
+    @DisplayName("폴더 수정 시 존재하지 않는 폴더 ID를 입력하면 MyException이 발생한다")
     void updateFolder_FolderNotFound() {
         // given: findById 메서드에서 폴더를 찾지못한 경우 가정
-        when(folderRepository.findById(anyLong())).thenReturn(Optional.empty());
+        given(folderRepository.findById(anyLong())).willReturn(Optional.empty());
 
         // when & then: 예외가 발생하는지 검증
         assertThatThrownBy(() -> folderService.updateFolder(1L, folderDto))
@@ -104,4 +92,7 @@ class FolderServiceTest {
         // folderRepository의 findById가 한 번 호출되었는지 검증
         verify(folderRepository, times(1)).findById(anyLong());
     }
+
+
+
 }
