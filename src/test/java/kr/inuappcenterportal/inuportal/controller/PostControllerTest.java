f45682e31ed57dad21691f31198064d051af257c
Kimseungin0529 <whffkaos007@naver.com>
2025-01-09 16:04:31 +0900

    refactor : profile test로 변경

--- src/test/java/kr/inuappcenterportal/inuportal/controller/PostControllerTest.java
+++ src/test/java/kr/inuappcenterportal/inuportal/controller/PostControllerTest.java
@@ -22,6 +22,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 class PostControllerTest {
 
     @Autowired
