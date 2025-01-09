019b2d9754abd823cbffac1e8d367d9006149f58
Kimseungin0529 <whffkaos007@naver.com>
2025-01-09 13:16:33 +0900

    feat : CI PR 반영을 위한 쓰레기값 테스트 작성

---
+++ src/test/java/module/BasicTest.java
@@ -0,0 +1,17 @@
+package module;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BasicTest {
+
+
+    @DisplayName("CI 파이프라인을 통해 PR에 테스트 표시 점검을 위한 테스트 입니다.")
+    @Test
+    void test() {
+        // given
+        // when
+        // then
+    }
+
+}
